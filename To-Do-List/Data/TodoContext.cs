e2e18d9e1064bcee44f3d63a9065671da156cb0b
Alif829 <bsse1120@iit.du.ac.bd>
2023-02-16 15:59:28 +0600

    Add project files.

---
+++ To-Do-List/Data/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using To_Do_List.Models;
+
+namespace To_Do_List.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+
+}
---
+++ To-Do-List/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace To_Do_List.Models
+{
+    public class TodoItem
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+}
